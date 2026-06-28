691559d94f15b091f8e98142cbf0c934a7c356b5
eduardovsilvaaa-sys <eduardovsilvaaa@gmail.com>
2026-06-28 16:51:02 -0300

    fix: baseURL do axios apontando para o render

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,18 +1,25 @@
-import axios from 'axios'
+import axios, { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: (import.meta.env.VITE_API_URL as string) || 'https://mapa-hexa.onrender.com',
 })
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem('token')
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error)
+  }
+)
 
 api.interceptors.response.use(
-  (res) => res,
-  (error) => {
+  (res: AxiosResponse) => res,
+  (error: AxiosError) => {
     if (error.response?.status === 401) {
       localStorage.clear()
       window.location.href = '/'
